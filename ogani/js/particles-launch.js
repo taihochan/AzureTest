97f37783e73d9862b13beebcdc1167c4ff574f91
taihochan <jake831210@gmail.com>
2020-05-26 11:46:19 +0800

    particle_finish

--- ogani/js/particles-launch.js
+++ ogani/js/particles-launch.js
@@ -123,7 +123,7 @@ stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
 count_particles = document.querySelector('.js-count-particles');
-update = function() {
+update = function () {
     stats.begin();
     stats.end();
     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
